b69bbc99e03d811b39aa99f73fa4686d986d9395
shriramrathod470 <shriramrathod47@gmail.com>
2022-08-23 01:07:12 +0530

    gfyvh

--- src/components/App.js
+++ src/components/App.js
@@ -2,7 +2,7 @@ import React, { Component, useState } from "react";
 import '../styles/App.css';
 import Heading from './Heading';
 import SubHeading from './SubHeading';
-import InputQuery from './nputQuery';
+import InputQuery from './InputQuery';
 import  SubmitButton from './SubmitButton';
 const App = () => {
   return (
